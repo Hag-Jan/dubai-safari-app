0c05843f056c1736a28da2ad2d25f19114616f9f
Hagop Jan <hagopjan@HagopsMcBookPro.lan>
2023-11-13 11:52:29 +0100

    Modify SingleTour component and its styles

--- src/pages/tour/SingleTour.jsx
+++ src/pages/tour/SingleTour.jsx
@@ -23,6 +23,26 @@ const SingleTour = () => {
                     Activity Provider : <span>Hag</span>
                 </div>
             </div>
+            <div className="tour-img-wrapper">
+                <img src={tour.image} alt={tour.title} className="tour-img" />
+                <div className="book-now">
+                        <div className="book-now-header">Save up to 35%</div>
+                    <div className="book-now-body">
+                        <div className="book-now-booked">Booked 42 times yesterday</div>
+                        <div className="book-now-price-wrapper">
+                            <div className="book-now-price">
+                                <b>${tour.priceFrom}</b>
+                                <p>per person</p>
+                            </div>
+                            <button className="book-now-btn">Book Now</button>
+                        </div>
+                        <p className="book-now-more-info">
+                            <i className="bi bi-calendar-check"></i>
+                            <span>Reserve now and pay later to book your spot and pay nothing today</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }

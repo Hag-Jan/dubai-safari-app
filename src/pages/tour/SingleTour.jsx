d8e8f6fa952bdb41595f4ec1b3ec8444ce61c71a
Hagop Jan <hagopjan@HagopsMcBookPro.lan>
2023-11-13 12:32:36 +0100

    Modify SingleTour component, its styles, and add BookNow component

--- src/pages/tour/SingleTour.jsx
+++ src/pages/tour/SingleTour.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { toursList } from "../../data";
 import "./single-tour.css"
 import Rating from "../../components/tours/Rating";
+import BookNow from "./BookNow";
 
 const SingleTour = () => {
     const { id } = useParams()
@@ -25,22 +26,48 @@ const SingleTour = () => {
             </div>
             <div className="tour-img-wrapper">
                 <img src={tour.image} alt={tour.title} className="tour-img" />
-                <div className="book-now">
-                        <div className="book-now-header">Save up to 35%</div>
-                    <div className="book-now-body">
-                        <div className="book-now-booked">Booked 42 times yesterday</div>
-                        <div className="book-now-price-wrapper">
-                            <div className="book-now-price">
-                                <b>${tour.priceFrom}</b>
-                                <p>per person</p>
-                            </div>
-                            <button className="book-now-btn">Book Now</button>
-                        </div>
-                        <p className="book-now-more-info">
-                            <i className="bi bi-calendar-check"></i>
-                            <span>Reserve now and pay later to book your spot and pay nothing today</span>
-                        </p>
+                <BookNow priceForm={tour.priceFrom} />
+            </div>
+            <p className="tour-description">
+                Discover a different side of Dubai an a fun-filled safari across the
+                desert. Try sandboarding, dune bashing and a camel ride, with the option
+                to add an electifying quad bike ride or immersive Al Khayma Camp dinner
+                experience to your adventure.
+            </p>
+            <div className="tour-more-information">
+                <h2 className="tour-more-title">About this activity</h2>
+                <div className="tour-info-item">
+                    <div className="tour-info-item-title">
+                        <i style={{ color: '#27ae60' }} className="bi bi-calndar3">Free Cancelation</i>
                     </div>
+                    <p className="tour-info-item-desc">
+                        Candel up to 24 hours in advance to recieve a full refund
+                    </p>
+                </div>
+                <div className="tour-info-item">
+                    <div className="tour-info-item-title">
+                        <i style={{ color: '#2980b9' }} className="bi bi-calendar-plus-fill">Reserve now & pay later</i>
+                    </div>
+                    <p className="tour-info-item-desc">Keep your travel plan flexible -- book your spot and pay nothing</p>
+                </div>
+                <div className="tour-info-item">
+                    <div className="tour-info-item-title">
+                        <i style={{ color: '#e74c3c' }} className="bi bi-virus">Covid -23 precautions</i>
+                    </div>
+                    <p className="tour-info-item-desc">Special health and safety measures are in place. Check your activity
+                        voucher once you book for full details.</p>
+                </div>
+                <div className="tour-info-item">
+                    <div className="tour-info-item-title">
+                        <i style={{ color: '#8e44ad' }} className="bi bi-clock-history">Duration 4 - 7 hours</i>
+                    </div>
+                    <p className="tour-info-item-desc">Check availability to see staring times.</p>
+                </div>
+                <div className="tour-info-item">
+                    <div className="tour-info-item-title">
+                        <i style={{ color: '#2c3e50' }} className="bi bi-person-check">Live tour guide</i>
+                    </div>
+                    <p className="tour-info-item-desc">English</p>
                 </div>
             </div>
         </section>
